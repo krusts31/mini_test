722251d280efd880f0464de0cf916537e47b6054
krusts31 <alkrusts@student.codam.nl>
2022-06-15 23:59:04 +0200

    working on the construtor prining

--- mini_test.hpp
+++ mini_test.hpp
@@ -40,7 +40,23 @@ namespace mini_test
 			std::string			_name;
 			std::string			_typename;
 			std::string			_constructor;
+			int 				_c;
+			int 				_size_of_vec;
 		public:
+			void	get_constructor(void)
+			{
+				if (_c == fill)
+					std::cout << "(" << _size_of_vec << ").";
+				if (_c == fill_2)
+				{
+					if (_typename.compare("char *") != 0)
+						std::cout << "(" << _size_of_vec << ", " << _arg_2 << ").";
+				}
+				if (_c == range)
+					std::cout << "coming sooon";
+				if (_c == copy)
+					std::cout << "coming sooon";
+			}
 			explicit mini_test(std::string name,
 					bool (*f)(std::vector<T> *, ft::vector<T> *),
 					ft::vector<T> *ft_vec,
@@ -52,16 +68,8 @@ namespace mini_test
 			{
 				_typename = typeid(T).name();
 				_name = name;
-				if (c == def)
-					_constructor = "DEFAULT ";
-				if (c == fill)
-					_constructor = "FILL ";
-				if (c == fill_2)
-					_constructor = "FILL_WITH_2_ARGS ";
-				if (c == range)
-					_constructor = "RANGE ";
-				if (c == copy)
-					_constructor = "COPY ";
+				_size_of_vec = size_of_vec;
+				_c = c;
 				if (_typename.compare("PPc") == 0)
 					_typename = "char **";
 				if (_typename.compare("Pc") == 0)
@@ -76,9 +84,7 @@ namespace mini_test
 					_typename = "char";
 				if (_typename.compare("d") == 0)
 					_typename = "double";
-					std::cout << YELLOW
-					<< 	_constructor
-					<< BOLDWHITE
+					std::cout << BOLDWHITE
 					<< "ft::vector" 
 					<< BOLDMAGENTA 
 					<< "<"
@@ -86,11 +92,11 @@ namespace mini_test
 					<< _typename 
 					<< BOLDMAGENTA
 					<< ">"
-					<< BOLDWHITE
-					<< "("
-					<< ft_vec->size()
-					<< ")."
-					<< _name 
+					<< BOLDWHITE;
+
+					get_constructor();
+
+					std::cout << _name 
 					<< RESET
 					<< " == " 
 					<< BOLDWHITE
@@ -101,11 +107,9 @@ namespace mini_test
 					<< _typename 
 					<< BOLDMAGENTA
 					<< ">"
-					<< BOLDWHITE
-					<< "("
-					<< size_of_vec
-					<< ")."
-					<< _name 
+					<< BOLDWHITE;
+					get_constructor();
+					std::cout << _name 
 					<< RESET;
 				if (f(std_vec, ft_vec))
 				{
@@ -131,7 +135,7 @@ namespace mini_test
 						<< ">"
 						<< BOLDWHITE
 						<< "("
-						<< ft_vec->size()
+						<< size_of_vec
 						<< ")."
 						<< _name 
 						<< RESET
@@ -192,7 +196,7 @@ namespace mini_test
 						<< ">"
 						<< BOLDWHITE
 						<< "("
-						<< ft_vec->size()
+						<< size_of_vec
 						<< ")."
 						<< _name 
 						<< RESET
@@ -216,7 +220,7 @@ namespace mini_test
 						<< ">"
 						<< BOLDWHITE
 						<< "("
-						<< ft_vec->size()
+						<< size_of_vec
 						<< ")."
 						<< _name 
 						<< RESET
