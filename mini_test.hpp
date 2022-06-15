4c32b67eea5b591f602285185a3b0d2e46eb7d02
Abdoulaye <abba@student.codam.nl>
2022-06-16 00:14:59 +0200

    made some improvement

--- mini_test.hpp
+++ mini_test.hpp
@@ -6,30 +6,30 @@
 # include <string>
 # include <csignal>
 # include "vector.hpp"
-
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
-#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
-#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
-#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
-#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+# include "color.hpp"
 
 namespace mini_test
 {
-	enum constructor{def, fill, fill_2, range, copy};
 	size_t	g_passed = 0;
 	size_t	g_failed = 0;
+
+    String list[] = {
+            "DEFAULT ",
+            "FILL ",
+            "FILL_WITH_2_ARGS ",
+            "RANGE",
+            "COPY"
+    };
+
+    String list1[][] = {
+            {"PPc", "char **"},
+            {"Pc", "char **"},
+            {"i", "int"},
+            {"f", "float"},
+            {"NSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEE", "std::string"},
+            {"c", "char"},
+            {"d", "double"}
+    };
 
 	template <class T>
 	class mini_test
@@ -52,30 +52,24 @@ namespace mini_test
 			{
 				_typename = typeid(T).name();
 				_name = name;
-				if (c == def)
-					_constructor = "DEFAULT ";
-				if (c == fill)
-					_constructor = "FILL ";
-				if (c == fill_2)
-					_constructor = "FILL_WITH_2_ARGS ";
-				if (c == range)
-					_constructor = "RANGE ";
-				if (c == copy)
-					_constructor = "COPY ";
-				if (_typename.compare("PPc") == 0)
-					_typename = "char **";
-				if (_typename.compare("Pc") == 0)
-					_typename = "char *";
-				if (_typename.compare("i") == 0)
-					_typename = "int";
-				if (_typename.compare("f") == 0)
-					_typename = "float";
-				if (_typename.compare("NSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEE") == 0)
-					_typename = "std::string";
-				if (_typename.compare("c") == 0)
-					_typename = "char";
-				if (_typename.compare("d") == 0)
-					_typename = "double";
+
+                for (int i = 0; i < sizeof(list) / sizeof(list[0]); i++)
+                {
+                    if (c == i)
+                    {
+                        _constructor = list[i];
+                        break ;
+                    }
+                }
+
+                for (int i = 0; i < 7; i++)
+                {
+                    if (_typename.compare(list1[i][i]))
+                    {
+                        _typename = list1[i][1];
+                        break ;
+                    }
+                }
 					std::cout << YELLOW
 					<< 	_constructor
 					<< BOLDWHITE
@@ -139,9 +133,7 @@ namespace mini_test
 #endif
 				}
 			}
-			/*
-			 *	ft::vector<int>(1, 2).begin() == std::vector<int>(1, 2).begin()
-			 */
+
 			explicit mini_test(std::string name,
 					bool (*f)(ft::vector<T> *, ft::vector<T> *),
 					ft::vector<T> *ft_vec,
@@ -153,30 +145,24 @@ namespace mini_test
 			{
 				_typename = typeid(T).name();
 				_name = name;
-				if (c == def)
-					_constructor = "DEFAULT ";
-				if (c == fill)
-					_constructor = "FILL ";
-				if (c == fill_2)
-					_constructor = "FILL_WITH_2_ARGS ";
-				if (c == range)
-					_constructor = "RANGE";
-				if (c == copy)
-					_constructor = "COPY";
-				if (_typename.compare("PPc") == 0)
-					_typename = "char **";
-				if (_typename.compare("Pc") == 0)
-					_typename = "char *";
-				if (_typename.compare("i") == 0)
-					_typename = "int";
-				if (_typename.compare("f") == 0)
-					_typename = "float";
-				if (_typename.compare("NSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEE") == 0)
-					_typename = "std::string";
-				if (_typename.compare("c") == 0)
-					_typename = "char";
-				if (_typename.compare("d") == 0)
-					_typename = "double";
+
+                for (int i = 0; i < sizeof(list) / sizeof(list[0]); i++)
+                {
+                    if (c == i)
+                    {
+                        _constructor = list[i];
+                        break ;
+                    }
+                }
+
+                for (int i = 0; i < 7; i++)
+                {
+                    if (_typename.compare(list1[i][i]))
+                    {
+                        _typename = list1[i][1];
+                        break ;
+                    }
+                }
 				if (f(ft_vec_2, ft_vec))
 				{
 					g_passed++;
